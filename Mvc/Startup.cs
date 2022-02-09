b84771ee90573a4d69a8be83a275f330106c209c
Kristian <kristian.a.boban@valtech.com>
2022-02-09 15:53:24 +0100

    Lab for .NET + MVC

--- Mvc/Startup.cs
+++ Mvc/Startup.cs
@@ -19,6 +19,8 @@ namespace Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,10 +36,7 @@ namespace Mvc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
